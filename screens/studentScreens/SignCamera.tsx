c0438761e238b3aab133205e59f9635507c427cd
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2024-07-16 23:32:54 +0000

    added face_registration route to the user registration stages

---
+++ screens/studentScreens/SignCamera.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ThemedText } from "../../contexts/ThemedText";
+import { ThemedView } from "../../contexts/ThemedView";
+
+const FaceRegistration = () => {
+  return (
+    <ThemedView className="flex-1 flex justify-center items-center">
+      <ThemedText>Face Registration Here</ThemedText>
+    </ThemedView>
+  );
+};
+
+export default FaceRegistration;
